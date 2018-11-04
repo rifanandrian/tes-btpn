76892f9d4f9af07aa3d47836b55baca701515f77
rifanandrian <rifan_andrian@yahoo.co.id>
2018-11-04 13:31:43 +0700

    Menambahkan folder helpers yang terdiri dari error.interceptor yang berfugnsi unutk membatasi respon dari api untuk mengecek terjadinya error atau tidak, fake-backend berfungsi untuk menyediakan contoh data tanpa adanya backend, jwt.interceptor berfungsi untuk membatasi request dari aplikasi untuk ditambhkan pada jwt token atuentikasi untuk memberikan hak pada user yang login

---
+++ src/app/_helpers/fake-backend.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, mergeMap, materialize, dematerialize } from 'rxjs/operators';
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+
+    constructor() { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let testUser = { id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' };
+
+        return of(null).pipe(mergeMap(() => {
+
+            // authenticate
+            if (request.url.endsWith('/users/authenticate') && request.method === 'POST') {
+                if (request.body.username === testUser.username && request.body.password === testUser.password) {
+                    let body = {
+                        id: testUser.id,
+                        username: testUser.username,
+                        firstName: testUser.firstName,
+                        lastName: testUser.lastName,
+                        token: 'fake-jwt-token'
+                    };
+                    return of(new HttpResponse({ status: 200, body }));
+                } else {
+                    return throwError({ error: { message: 'Username or password is incorrect' } });
+                }
+            }
+
+            if (request.url.endsWith('/users') && request.method === 'GET') {
+                if (request.headers.get('Authorization') === 'Bearer fake-jwt-token') {
+                    return of(new HttpResponse({ status: 200, body: [testUser] }));
+                } else {
+                    return throwError({ error: { message: 'Unauthorised' } });
+                }
+            }
+
+            return next.handle(request);
+            
+        }))
+
+        .pipe(materialize())
+        .pipe(delay(500))
+        .pipe(dematerialize());
+    }
+}
+
+export let fakeBackendProvider = {
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
+};
