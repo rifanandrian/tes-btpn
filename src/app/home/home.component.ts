4688157e6e5900f624bf5e23bce957d28a8c9a61
rifanandrian <rifan_andrian@yahoo.co.id>
2018-11-05 13:30:19 +0700

    Update tampilan hime.html, edit home untuk mendapatkan data yang diiginkan

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,16 +3,21 @@ import { first } from 'rxjs/operators';
 
 import { User } from '../_models';
 import { UserService } from '../_services';
+import { AuthenticationService } from '../_services';
 
 @Component({templateUrl: 'home.component.html'})
 export class HomeComponent implements OnInit {
     users: User[] = [];
+    user: any;
 
-    constructor(private userService: UserService) {}
+    constructor(private userService: UserService, private authServ: AuthenticationService ) {}
 
     ngOnInit() {
+        // console.log(this.authServ.getUser());
+        this.user = this.authServ.getUser();
         this.userService.getAll().pipe(first()).subscribe(users => { 
-            this.users = users; 
+            this.users = users;
+            // console.log(this.users); 
         });
     }
 }
